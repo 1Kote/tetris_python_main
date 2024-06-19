b3bcc42aa9e4e5e43fa2831f0f1275b015034364
1Kote <thepvpgod1@gmail.com>
2024-06-19 17:06:02 -0300

    Parte da classe principal(tetris.py) foi refatorada e uma classe nova foi criada(game.py) para receber os metodos removidos da classe principal. Agora ao iniciar a janela do programa o jogo escolhe uma peça aleatoria da lista para aparecer em sua posição incial predefinida.

--- tetris.py
+++ tetris.py
@@ -1,11 +1,11 @@
 import pygame, sys
-from grid import Grid
-from blocks import *
+from game import Game
 
 pygame.init()
 
 #cor
 dark_blue = (44, 44, 127)
+
 #JANELA DO JOGO
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Tetris")
@@ -13,12 +13,7 @@ pygame.display.set_caption("Tetris")
 #relogio para o fps funcionar
 clock = pygame.time.Clock()
 
-#EXECUÇÃO DO METODO GRID
-game_grid = Grid()
-
-#MOSTRA UM BLOCO NA TELA(FINS DE DEBUG)
-block = OBlock()
-#block.move(4,3)
+game = Game()
 
 #LOOP PRINCIPAL 
 while True:
@@ -28,8 +23,7 @@ while True:
             sys.exit()
 #Desenho da tela
     screen.fill(dark_blue)
-    game_grid.draw(screen)
-    block.draw(screen)
+    game.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
